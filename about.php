e9b3b9cb61fbc0807b5a34460ad1a37e4114e80d
anhtuet1306 <dangtuyet969@gmail.com>
2026-08-23 21:47:43 +0700

    Update about.php
    them tim-kiem

--- about.php
+++ about.php
@@ -26,6 +26,7 @@ function e(string $value): string
         <nav>
             <a href="index.php">Trang chủ</a>
             <a href="about.php">Giới thiệu nhóm</a>
+            <a href="tim-kiem.php">Tìm kiếm</a>
         </nav>
 
         <section class="intro small">
