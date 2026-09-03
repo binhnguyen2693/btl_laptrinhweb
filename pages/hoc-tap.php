38d2cad53bc59ca0f404fca8c62ebf049a51df28
anhtuet1306 <dangtuyet969@gmail.com>
2026-09-03 22:03:54 +0700

    Create hoc-tap.php

---
+++ pages/hoc-tap.php
@@ -0,0 +1,29 @@
+<?php include '../includes/header.php'; ?>
+
+<section class="intro small">
+    <h1>HỌC TẬP & NGHIÊN CỨU</h1>
+    <p>Kiến thức, nghiên cứu và kinh nghiệm học tập dành cho sinh viên khoa.</p>
+</section>
+
+<div class="features">
+    <article>
+        <h2>Kinh nghiệm học tốt các môn chuyên ngành</h2>
+        <p>Một số kinh nghiệm và phương pháp giúp sinh viên chủ động xây dựng kế hoạch học tập, nâng cao khả năng tự học...</p>
+        <small>10/08/2026</small><br><br>
+        <a href="chi-tiet-bai-viet.php" class="button">Xem bài</a>
+    </article>
+    <article>
+        <h2>Sinh viên và cơ hội tham gia nghiên cứu khoa học</h2>
+        <p>Tìm hiểu các chương trình nghiên cứu, đề tài học thuật và cơ hội tham gia cùng giảng viên để tích lũy kinh nghiệm...</p>
+        <small>10/08/2026</small><br><br>
+        <a href="chi-tiet-bai-viet.php" class="button">Xem bài</a>
+    </article>
+    <article>
+        <h2>5 kỹ năng sinh viên cần chuẩn bị trước khi thực tập</h2>
+        <p>Những kỹ năng quan trọng giúp sinh viên chuẩn bị tốt hơn cho kỳ thực tập, từ giao tiếp đến làm việc nhóm...</p>
+        <small>10/08/2026</small><br><br>
+        <a href="chi-tiet-bai-viet.php" class="button">Xem bài</a>
+    </article>
+</div>
+
+<?php include '../includes/footer.php'; ?>
