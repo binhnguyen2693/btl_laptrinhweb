cc664c68b268c63e309763cfc6bf80f6d2e93d8f
anhtuet1306 <dangtuyet969@gmail.com>
2026-09-03 22:04:03 +0700

    Create chi-tiet-thay-doi.php

---
+++ pages/chi-tiet-thay-doi.php
@@ -0,0 +1,40 @@
+<?php include '../includes/header.php'; ?>
+
+<section class="intro small">
+    <h1>THÔNG TIN THAY ĐỔI LỊCH HỘI THẢO</h1>
+    <p>Cập nhật lần cuối: 16/08/2026 • Người đăng: Ban tổ chức • 325 lượt xem</p>
+</section>
+
+<div class="box">
+    <h2>Lịch hội thảo nghiên cứu khoa học được chuyển sang ngày 25/08/2026</h2>
+    <div class="table-wrapper">
+        <table>
+            <thead>
+                <tr>
+                    <th>Nội dung</th>
+                    <th>Chi tiết</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><b>Thời gian cũ</b></td>
+                    <td>20/08/2026</td>
+                </tr>
+                <tr>
+                    <td><b>Thời gian mới</b></td>
+                    <td><b style="color: #b91c1c;">25/08/2026</b></td>
+                </tr>
+                <tr>
+                    <td><b>Đối tượng ảnh hưởng</b></td>
+                    <td>Sinh viên đã đăng ký tham dự Hội thảo NCKH 2026</td>
+                </tr>
+                <tr>
+                    <td><b>Nội dung thay đổi</b></td>
+                    <td>Ban tổ chức thông báo chuyển lịch sang ngày 25/08/2026 để hoàn tất công tác chuẩn bị.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include '../includes/footer.php'; ?>
