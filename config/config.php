4b7afc01da41fb535fa6f63a082cc016711f5a0e
tranquynh03 <224001828@daihocthudo.edu.vn>
2026-09-06 15:10:04 +0700

    update

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+define('SITE_NAME', 'Cổng thông tin khoa');
+define('BASE_URL', '/btl_laptrinhweb/');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'web_nhom');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+?>
